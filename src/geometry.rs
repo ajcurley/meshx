61be3c028c5bc2f3c84d4fabec743473c0f14232
Alex Curley <=>
2024-04-17 11:04:25 -0400

    Implemented Triangle struct

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,8 +1,10 @@
 pub mod aabb;
 pub mod sphere;
+pub mod triangle;
 pub mod vector3;
 
 // Re-exports
 pub use aabb::Aabb;
 pub use sphere::Sphere;
+pub use triangle::Triangle;
 pub use vector3::Vector3;
---
+++ src/geometry/triangle.rs
@@ -0,0 +1,70 @@
+use crate::geometry::{Aabb, Vector3};
+
+/// Triangle in three-dimensional Cartesian space
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Triangle {
+    p: Vector3,
+    q: Vector3,
+    r: Vector3,
+}
+
+impl Triangle {
+    /// Construct a Triangle from its vertices p, q, and r
+    pub fn new(p: Vector3, q: Vector3, r: Vector3) -> Triangle {
+        Triangle { p, q, r }
+    }
+
+    /// Compute the axis-aligned bounding box
+    pub fn aabb(&self) -> Aabb {
+        let mut min = Vector3::zeros();
+        let mut max = Vector3::zeros();
+
+        for i in 0..3 {
+            min[i] = self.p[i].min(self.q[i]).min(self.r[i]);
+            max[i] = self.p[i].max(self.q[i]).max(self.r[i]);
+        }
+
+        Aabb::from_bounds(min, max)
+    }
+
+    /// Compute the area
+    pub fn area(&self) -> f64 {
+        self.normal().mag() * 0.5
+    }
+
+    /// Compute the normal vector (non-normalized)
+    pub fn normal(&self) -> Vector3 {
+        let u = self.q - self.p;
+        let v = self.r - self.p;
+        Vector3::cross(&u, &v)
+    }
+
+    /// Compute the unit normal vector
+    pub fn unit_normal(&self) -> Vector3 {
+        self.normal().unit()
+    }
+}
+
+impl std::ops::Index<usize> for Triangle {
+    type Output = Vector3;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        match index {
+            0 => &self.p,
+            1 => &self.q,
+            2 => &self.r,
+            _ => panic!("index out of range"),
+        }
+    }
+}
+
+impl std::ops::IndexMut<usize> for Triangle {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        match index {
+            0 => &mut self.p,
+            1 => &mut self.q,
+            2 => &mut self.r,
+            _ => panic!("index out of range"),
+        }
+    }
+}
