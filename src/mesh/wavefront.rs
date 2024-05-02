373a11531ff5b30d8a7d1dc6b0d49d1a32c1b5cf
Alex Curley <=>
2024-05-02 08:31:57 -0400

    Added ObjWriter

--- src/mesh/wavefront.rs
+++ src/mesh/wavefront.rs
@@ -4,6 +4,8 @@ use std::io::prelude::*;
 use std::path::Path;
 
 use flate2::read::GzDecoder;
+use flate2::write::GzEncoder;
+use flate2::Compression;
 
 #[derive(Debug, Clone)]
 pub struct ObjReader {
@@ -157,6 +159,162 @@ impl ObjReader {
         let group = ObjGroup::new(name);
         self.groups.push(group);
         Ok(())
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct ObjWriter {
+    vertices: Vec<ObjVertex>,
+    faces: Vec<ObjFace>,
+    lines: Vec<ObjLine>,
+    groups: Vec<ObjGroup>,
+}
+
+impl ObjWriter {
+    /// Construct an ObjWriter
+    pub fn new() -> ObjWriter {
+        ObjWriter::default()
+    }
+
+    /// Set the vertices
+    pub fn set_vertices(&mut self, vertices: Vec<ObjVertex>) {
+        self.vertices = vertices;
+    }
+
+    /// Set the faces
+    pub fn set_faces(&mut self, faces: Vec<ObjFace>) {
+        self.faces = faces;
+    }
+
+    /// Set the lines
+    pub fn set_lines(&mut self, lines: Vec<ObjLine>) {
+        self.lines = lines;
+    }
+
+    /// Set the groups
+    pub fn set_groups(&mut self, groups: Vec<ObjGroup>) {
+        self.groups = groups;
+    }
+
+    /// Write the mesh to file
+    pub fn write(&self, filename: &str) -> std::io::Result<()> {
+        let mut data = String::new();
+        let mut group_faces: Vec<Vec<usize>> = vec![vec![]; self.groups.len() + 1];
+        let mut group_lines: Vec<Vec<usize>> = vec![vec![]; self.groups.len() + 1];
+
+        // Assign the faces to a group. If a face does not have a group, assign
+        // it to the default group at index 0.
+        for (i, face) in self.faces.iter().enumerate() {
+            if let Some(group) = face.group {
+                group_faces[group + 1].push(i);
+            } else {
+                group_faces[0].push(i);
+            }
+        }
+
+        // Assign the lines to a group. If a line does not have a group, assign
+        // it to the default group at index 0.
+        for (i, line) in self.lines.iter().enumerate() {
+            if let Some(group) = line.group {
+                group_lines[group + 1].push(i);
+            } else {
+                group_lines[0].push(i);
+            }
+        }
+
+        // Format all the vertices.
+        for vertex in self.vertices.iter() {
+            let entry = self.format_vertex(vertex);
+            data.push_str(&entry);
+        }
+
+        // Format the faces for the default (unnamed) group.
+        for i in group_faces[0].iter() {
+            let entry = self.format_face(&self.faces[*i]);
+            data.push_str(&entry);
+        }
+
+        // Format the lines for the default (unnamed) group.
+        for i in group_lines[0].iter() {
+            let entry = self.format_line(&self.lines[*i]);
+            data.push_str(&entry);
+        }
+
+        // For each group, format the group followed by all of its assigned
+        // faces and lines.
+        for (i, group) in self.groups.iter().enumerate() {
+            let entry = self.format_group(group);
+            data.push_str(&entry);
+
+            for j in group_faces[i + 1].iter() {
+                let entry = self.format_face(&self.faces[*j]);
+                data.push_str(&entry);
+            }
+
+            for j in group_lines[i + 1].iter() {
+                let entry = self.format_line(&self.lines[*j]);
+                data.push_str(&entry);
+            }
+        }
+
+        // Write the data to file.
+        let mut file = File::create(filename)?;
+        let content = data.as_bytes();
+
+        if self.is_compressed(filename) {
+            let mut encoder = GzEncoder::new(&mut file, Compression::default());
+            encoder.write_all(&content)?;
+        } else {
+            file.write_all(&content)?;
+        }
+
+        Ok(())
+    }
+
+    /// Format a vertex to an entry
+    fn format_vertex(&self, vertex: &ObjVertex) -> String {
+        format!("v {} {} {}\n", vertex.x, vertex.y, vertex.z)
+    }
+
+    /// Format a face to an entry
+    fn format_face(&self, face: &ObjFace) -> String {
+        let vertices = face
+            .vertices
+            .iter()
+            .map(|v| (v + 1).to_string())
+            .collect::<Vec<String>>()
+            .join(" ");
+
+        format!("f {}\n", vertices)
+    }
+
+    /// Format a line to an entry
+    fn format_line(&self, line: &ObjLine) -> String {
+        let vertices = line
+            .vertices
+            .iter()
+            .map(|v| (v + 1).to_string())
+            .collect::<Vec<String>>()
+            .join(" ");
+
+        format!("l {}\n", vertices)
+    }
+
+    /// Format a group to an entry
+    fn format_group(&self, group: &ObjGroup) -> String {
+        format!("g {}\n", group.name)
+    }
+
+    /// Get if the input file is compressed
+    fn is_compressed(&self, filename: &str) -> bool {
+        let path = Path::new(filename);
+        let extension = path.extension().and_then(OsStr::to_str);
+
+        if let Some(ext) = extension {
+            return ext.to_lowercase() == "gz";
+        }
+
+        false
     }
 }
 
@@ -317,5 +475,91 @@ mod test {
         assert_eq!(reader.vertices().len(), 8);
         assert_eq!(reader.faces().len(), 12);
         assert_eq!(reader.groups().len(), 6);
+    }
+
+    #[test]
+    fn test_obj_writer() {
+        let path = "tests/fixtures/box.obj";
+        let mut reader = ObjReader::new(&path);
+        reader.read().unwrap();
+
+        let out_path = "/tmp/box.obj";
+        let mut writer = ObjWriter::new();
+        writer.set_vertices(reader.vertices);
+        writer.set_faces(reader.faces);
+        writer.set_groups(reader.groups);
+        writer.write(out_path).unwrap();
+
+        let mut expected_content = String::new();
+        let mut actual_content = String::new();
+
+        File::open(&path)
+            .unwrap()
+            .read_to_string(&mut expected_content)
+            .unwrap();
+
+        File::open(&out_path)
+            .unwrap()
+            .read_to_string(&mut actual_content)
+            .unwrap();
+
+        assert_eq!(actual_content, expected_content);
+    }
+
+    #[test]
+    fn test_obj_writer_gzip() {
+        let path = "tests/fixtures/box.obj";
+        let mut reader = ObjReader::new(&path);
+        reader.read().unwrap();
+
+        let out_path = "/tmp/box.obj.gz";
+        let mut writer = ObjWriter::new();
+        writer.set_vertices(reader.vertices);
+        writer.set_faces(reader.faces);
+        writer.set_groups(reader.groups);
+        writer.write(out_path).unwrap();
+
+        let mut expected_content = String::new();
+        let mut actual_content = String::new();
+
+        File::open(&path)
+            .unwrap()
+            .read_to_string(&mut expected_content)
+            .unwrap();
+
+        GzDecoder::new(File::open(&out_path).unwrap())
+            .read_to_string(&mut actual_content)
+            .unwrap();
+
+        assert_eq!(actual_content, expected_content);
+    }
+
+    #[test]
+    fn test_obj_writer_groups() {
+        let path = "tests/fixtures/box_groups.obj";
+        let mut reader = ObjReader::new(&path);
+        reader.read().unwrap();
+
+        let out_path = "/tmp/box_groups.obj";
+        let mut writer = ObjWriter::new();
+        writer.set_vertices(reader.vertices);
+        writer.set_faces(reader.faces);
+        writer.set_groups(reader.groups);
+        writer.write(out_path).unwrap();
+
+        let mut expected_content = String::new();
+        let mut actual_content = String::new();
+
+        File::open(&path)
+            .unwrap()
+            .read_to_string(&mut expected_content)
+            .unwrap();
+
+        File::open(&out_path)
+            .unwrap()
+            .read_to_string(&mut actual_content)
+            .unwrap();
+
+        assert_eq!(actual_content, expected_content);
     }
 }
