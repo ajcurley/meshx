65fd93faf416960bce1469060884c93d144c0eea
Alex Curley <=>
2024-05-18 12:08:06 -0400

    Fix fmt in unimplemented methods

--- src/geometry/collision/aabb_triangle.rs
+++ src/geometry/collision/aabb_triangle.rs
@@ -1,6 +1,6 @@
 use crate::geometry::{Aabb, Triangle};
 
 /// Check for a spatial intersection between an Aabb and Triangle
-pub fn intersects_aabb_triangle(aabb: &Aabb, triangle: &Triangle) -> bool {
+pub fn intersects_aabb_triangle(_aabb: &Aabb, _triangle: &Triangle) -> bool {
     unimplemented!()
 }
--- src/geometry/collision/sphere_triangle.rs
+++ src/geometry/collision/sphere_triangle.rs
@@ -1,6 +1,6 @@
 use crate::geometry::{Sphere, Triangle};
 
 /// Check for a spatial intersection between an Sphere and Triangle
-pub fn intersects_sphere_triangle(sphere: &Sphere, triangle: &Triangle) -> bool {
+pub fn intersects_sphere_triangle(_sphere: &Sphere, _triangle: &Triangle) -> bool {
     unimplemented!()
 }
