e0a20dea12b4dfcddfae5b717da65e32a288dafc
Alex Curley <=>
2024-05-18 11:37:41 -0400

    Added wrapper methods for Aabb intersections

--- src/geometry/aabb.rs
+++ src/geometry/aabb.rs
@@ -66,6 +66,26 @@ impl Aabb {
 
         Aabb::new(center, h)
     }
+
+    /// Check for a spatial intersection with an Aabb
+    pub fn intersects_aabb(&self, aabb: &Aabb) -> bool {
+        self.intersects(aabb)
+    }
+
+    /// Check for a spatial intersection with a Ray
+    pub fn intersects_ray(&self, ray: &Ray) -> bool {
+        self.intersects(ray)
+    }
+
+    /// Check for a spatial intersection with a Sphere
+    pub fn intersects_sphere(&self, sphere: &Sphere) -> bool {
+        self.intersects(sphere)
+    }
+
+    /// Check for a spatial intersection with a Vector3
+    pub fn intersects_vector3(&self, point: &Vector3) -> bool {
+        self.intersects(point)
+    }
 }
 
 impl Intersects<Aabb> for Aabb {
@@ -87,7 +107,7 @@ impl Intersects<Sphere> for Aabb {
 }
 
 impl Intersects<Vector3> for Aabb {
-    fn intersects(&self, v: &Vector3) -> bool {
-        collision::intersects_aabb_vector3(self, v)
+    fn intersects(&self, point: &Vector3) -> bool {
+        collision::intersects_aabb_vector3(self, point)
     }
 }
