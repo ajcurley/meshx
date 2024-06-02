632a2e1f0ae897f3240bc59cf2883afb75ca8ed8
Alex Curley <=>
2024-06-02 06:38:50 -0400

    Add access methods for Vertex

--- src/mesh/common.rs
+++ src/mesh/common.rs
@@ -1,3 +1,4 @@
+use pyo3::exceptions::PyIndexError;
 use pyo3::prelude::*;
 
 use crate::geometry::Vector3;
@@ -16,6 +17,41 @@ impl Vertex {
     #[new]
     pub fn new(x: f64, y: f64, z: f64) -> Vertex {
         Vertex { x, y, z }
+    }
+
+    /// Get the x-component
+    pub fn x(&self) -> f64 {
+        self.x
+    }
+
+    /// Get the y-component
+    pub fn y(&self) -> f64 {
+        self.y
+    }
+
+    /// Get the z-component
+    pub fn z(&self) -> f64 {
+        self.z
+    }
+
+    /// (Python) Get the component by index
+    pub fn __getitem__(&self, index: usize) -> PyResult<f64> {
+        if index > 2 {
+            return Err(PyIndexError::new_err("index out of range"));
+        }
+
+        Ok(self[index])
+    }
+
+    /// (Python) Set the component by index
+    pub fn __setitem__(&mut self, index: usize, value: f64) -> PyResult<()> {
+        if index > 2 {
+            return Err(PyIndexError::new_err("index out of range"));
+        }
+
+        self[index] = value;
+
+        Ok(())
     }
 }
 
