64c6ee5463fb2b273c9f646717d3df93cbc8c1ce
Alex Curley <=>
2024-06-07 13:37:02 -0400

    Initial handling of coplanar triangles

--- src/geometry/collision/triangle_triangle.rs
+++ src/geometry/collision/triangle_triangle.rs
@@ -1,14 +1,5 @@
 use crate::geometry::{Triangle, Vector3};
 
-#[derive(Debug, Copy, Clone, Default)]
-struct Interval {
-    a: f64,
-    b: f64,
-    c: f64,
-    x0: f64,
-    x1: f64,
-}
-
 /// Check for a spatial intersection two Triangles
 pub fn intersects_triangle_triangle(t1: &Triangle, t2: &Triangle) -> bool {
     const EPSILON: f64 = 1e-6;
@@ -76,12 +67,18 @@ pub fn intersects_triangle_triangle(t1: &Triangle, t2: &Triangle) -> bool {
     let up = Vector3::new(u0[index], u1[index], u2[index]);
 
     // Compute the interval for triangle 1
-    let (interval1, _coplanar) = compute_interval(vp, dv0, dv1, dv2, dv0dv1, dv0dv2);
-    // FIXME: handle coplanar
+    let (interval1, coplanar) = compute_interval(vp, dv0, dv1, dv2, dv0dv1, dv0dv2);
+
+    if coplanar {
+        return coplanar_tri_tri(n1, v0, v1, v2, u0, u1, u2);
+    }
 
     // Compute the interval for triangle 2
-    let (interval2, _coplanar) = compute_interval(up, du0, du1, du2, du0du1, du0du2);
-    // FIXME: handle coplanar
+    let (interval2, coplanar) = compute_interval(up, du0, du1, du2, du0du1, du0du2);
+
+    if coplanar {
+        return coplanar_tri_tri(n1, v0, v1, v2, u0, u1, u2);
+    }
 
     // Compute the overlap between the two intervals
     let xx = interval1.x0 * interval1.x1;
@@ -103,6 +100,15 @@ pub fn intersects_triangle_triangle(t1: &Triangle, t2: &Triangle) -> bool {
     }
 
     true
+}
+
+#[derive(Debug, Copy, Clone, Default)]
+struct Interval {
+    a: f64,
+    b: f64,
+    c: f64,
+    x0: f64,
+    x1: f64,
 }
 
 fn compute_interval(
@@ -156,4 +162,142 @@ fn compute_interval(
     }
 
     (interval, coplanar)
+}
+
+fn coplanar_tri_tri(
+    n: Vector3,
+    v0: Vector3,
+    v1: Vector3,
+    v2: Vector3,
+    u0: Vector3,
+    u1: Vector3,
+    u2: Vector3,
+) -> bool {
+    // Project onto an axis-aligned plane that maximizes the area of the
+    // triangles. Compute the indices i0 and i1.
+    let i0: usize;
+    let i1: usize;
+    let a = n.abs();
+
+    if a.x() > a.y() {
+        if a.x() > a.z() {
+            i0 = 1; // a.x() is the greatest
+            i1 = 2;
+        } else {
+            i0 = 0; // a.z() is the greatest
+            i1 = 1;
+        }
+    } else {
+        if a.z() > a.y() {
+            i0 = 0; // a.z() is the greatest
+            i1 = 1;
+        } else {
+            i0 = 0; // a.y() is the greatest
+            i1 = 2;
+        }
+    }
+
+    // Test all edges of triangle t1 against the edges of triangle t2.
+    if edge_against_tri_edges(v0, v1, u0, u1, u2, i0, i1) {
+        return true;
+    }
+
+    if edge_against_tri_edges(v1, v2, u0, u1, u2, i0, i1) {
+        return true;
+    }
+
+    if edge_against_tri_edges(v2, v0, u0, u1, u2, i0, i1) {
+        return true;
+    }
+
+    // Test if triangle t1 is totally contained in triangle t2 or if
+    // triangle t2 is totally contained in triangle t1.
+    if point_in_tri(v0, u0, u1, u2, i0, i1) {
+        return true;
+    }
+
+    if point_in_tri(u0, v0, v1, v2, i0, i1) {
+        return true;
+    }
+
+    false
+}
+
+fn edge_against_tri_edges(
+    v0: Vector3,
+    v1: Vector3,
+    u0: Vector3,
+    u1: Vector3,
+    u2: Vector3,
+    i0: usize,
+    i1: usize,
+) -> bool {
+    let ax = v1[i0] - v0[i0];
+    let ay = v1[i1] - v0[i1];
+
+    if edge_edge_test(v0, u0, u1, ax, ay, i0, i1) {
+        return true;
+    }
+
+    if edge_edge_test(v0, u1, u2, ax, ay, i0, i1) {
+        return true;
+    }
+
+    if edge_edge_test(v0, u2, u0, ax, ay, i0, i1) {
+        return true;
+    }
+
+    false
+}
+
+fn edge_edge_test(
+    v0: Vector3,
+    u0: Vector3,
+    u1: Vector3,
+    ax: f64,
+    ay: f64,
+    i0: usize,
+    i1: usize,
+) -> bool {
+    let bx = u0[i0] - u1[i0];
+    let by = u0[i1] - u1[i1];
+    let cx = v0[i0] - u0[i0];
+    let cy = v0[i1] - u0[i1];
+    let f = ay * bx - ax * by;
+    let d = by * cx - bx * cy;
+
+    if (f > 0. && d >= 0. && d <= f) || (f < 0. && d <= 0. && d >= f) {
+        let e = ax * cy - ay * cx;
+
+        if f > 0. {
+            if e >= 0. && e <= f {
+                return true;
+            }
+        } else {
+            if e <= 0. && e >= f {
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
+fn point_in_tri(v0: Vector3, u0: Vector3, u1: Vector3, u2: Vector3, i0: usize, i1: usize) -> bool {
+    let a = u1[i1] - u0[i1];
+    let b = -(u1[i0] - u0[i0]);
+    let c = -a * u0[i0] - b * u0[i1];
+    let d0 = a * v0[i0] + b * v0[i1] + c;
+
+    let a = u2[i1] - u1[i1];
+    let b = -(u2[i0] - u1[i0]);
+    let c = -a * u1[i0] - b * u1[i1];
+    let d1 = a * v0[i0] + b * v0[i1] + c;
+
+    let a = u0[i1] - u2[i1];
+    let b = -(u0[i0] - u2[i0]);
+    let c = -a * u2[i0] - b * u2[i1];
+    let d2 = a * v0[i0] + b * v0[i1] + c;
+
+    d0 * d1 > 0. && d0 * d2 > 0.
 }
