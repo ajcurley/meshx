4f0406ea932244c20f7becb86d5c7a009150b42d
Alex Curley <=>
2024-06-07 09:18:11 -0400

    Add tests

--- src/geometry/collision/aabb_triangle.rs
+++ src/geometry/collision/aabb_triangle.rs
@@ -93,7 +93,7 @@ fn axistest_x01(a: f64, b: f64, fa: f64, fb: f64, v0: Vector3, v2: Vector3, h: V
     let p2 = a * v2.y() - b * v2.z();
     let (min, max) = if p0 < p2 { (p0, p2) } else { (p2, p0) };
     let rad = fa * h.y() + fb * h.z();
-    min <= rad || max >= -rad
+    !(min > rad || max < -rad)
 }
 
 fn axistest_x2(a: f64, b: f64, fa: f64, fb: f64, v0: Vector3, v1: Vector3, h: Vector3) -> bool {
@@ -101,7 +101,7 @@ fn axistest_x2(a: f64, b: f64, fa: f64, fb: f64, v0: Vector3, v1: Vector3, h: Ve
     let p1 = a * v1.y() - b * v1.z();
     let (min, max) = if p0 < p1 { (p0, p1) } else { (p1, p0) };
     let rad = fa * h.y() + fb * h.z();
-    min <= rad || max >= -rad
+    !(min > rad || max < -rad)
 }
 
 fn axistest_y02(a: f64, b: f64, fa: f64, fb: f64, v0: Vector3, v2: Vector3, h: Vector3) -> bool {
@@ -109,7 +109,7 @@ fn axistest_y02(a: f64, b: f64, fa: f64, fb: f64, v0: Vector3, v2: Vector3, h: V
     let p2 = -a * v2.x() + b * v2.z();
     let (min, max) = if p0 < p2 { (p0, p2) } else { (p2, p0) };
     let rad = fa * h.x() + fb * h.z();
-    min <= rad || max >= -rad
+    !(min > rad || max < -rad)
 }
 
 fn axistest_y1(a: f64, b: f64, fa: f64, fb: f64, v0: Vector3, v1: Vector3, h: Vector3) -> bool {
@@ -117,7 +117,7 @@ fn axistest_y1(a: f64, b: f64, fa: f64, fb: f64, v0: Vector3, v1: Vector3, h: Ve
     let p1 = -a * v1.x() + b * v1.z();
     let (min, max) = if p0 < p1 { (p0, p1) } else { (p1, p0) };
     let rad = fa * h.x() + fb * h.z();
-    min <= rad || max >= -rad
+    !(min > rad || max < -rad)
 }
 
 fn axistest_z12(a: f64, b: f64, fa: f64, fb: f64, v1: Vector3, v2: Vector3, h: Vector3) -> bool {
@@ -125,7 +125,7 @@ fn axistest_z12(a: f64, b: f64, fa: f64, fb: f64, v1: Vector3, v2: Vector3, h: V
     let p2 = a * v2.x() - b * v2.y();
     let (min, max) = if p1 < p2 { (p1, p2) } else { (p2, p1) };
     let rad = fa * h.x() + fb * h.y();
-    min <= rad || max >= -rad
+    !(min > rad || max < -rad)
 }
 
 fn axistest_z0(a: f64, b: f64, fa: f64, fb: f64, v0: Vector3, v1: Vector3, h: Vector3) -> bool {
@@ -133,7 +133,7 @@ fn axistest_z0(a: f64, b: f64, fa: f64, fb: f64, v0: Vector3, v1: Vector3, h: Ve
     let p1 = a * v1.x() - b * v1.y();
     let (min, max) = if p0 < p1 { (p0, p1) } else { (p1, p0) };
     let rad = fa * h.x() + fb * h.y();
-    min <= rad || max >= -rad
+    !(min > rad || max < -rad)
 }
 
 fn findminmax(x0: f64, x1: f64, x2: f64) -> (f64, f64) {
@@ -175,9 +175,172 @@ fn plane_box_overlap(normal: Vector3, vert: Vector3, maxbox: Vector3) -> bool {
         }
     }
 
-    if Vector3::dot(&normal, &vmin) > 0. {
-        return false;
+    Vector3::dot(&normal, &vmin) <= 0. && Vector3::dot(&normal, &vmax) >= 0.
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn get_aabb() -> Aabb {
+        let center = Vector3::new(0.5, 0.5, 0.5);
+        let halfsize = Vector3::new(0.5, 0.5, 0.5);
+        Aabb::new(center, halfsize)
     }
 
-    Vector3::dot(&normal, &vmax) >= 0.
+    #[test]
+    fn test_aabb_triangle_ok_inside() {
+        let aabb = get_aabb();
+        let p = Vector3::new(0.1, 0.1, 0.1);
+        let q = Vector3::new(0.1, 0.1, 0.3);
+        let r = Vector3::new(0.1, 0.3, 0.1);
+        let triangle = Triangle::new(p, q, r);
+
+        let intersects = intersects_aabb_triangle(&aabb, &triangle);
+
+        assert!(intersects);
+    }
+
+    #[test]
+    fn test_aabb_triangle_fail_aabb() {
+        let aabb = get_aabb();
+        let p = Vector3::new(0., 0., 2.);
+        let q = Vector3::new(1., 0., 2.);
+        let r = Vector3::new(1., 1., 2.);
+        let triangle = Triangle::new(p, q, r);
+
+        let intersects = intersects_aabb_triangle(&aabb, &triangle);
+
+        assert!(!intersects);
+    }
+
+    #[test]
+    fn test_aabb_triangle_fail_plane() {
+        let aabb = get_aabb();
+        let p = Vector3::new(0.1, 1.1, 0.9);
+        let q = Vector3::new(0.5, 0.8, 1.5);
+        let r = Vector3::new(0.9, 1.1, 0.9);
+        let triangle = Triangle::new(p, q, r);
+
+        let intersects = intersects_aabb_triangle(&aabb, &triangle);
+
+        assert!(!intersects);
+    }
+
+    #[test]
+    fn test_aabb_triangle_fail_axis_e0_x01() {
+        let aabb = get_aabb();
+        let p = Vector3::new(0.5, 1.1, 0.9);
+        let q = Vector3::new(0.5, 0.8, 1.5);
+        let r = Vector3::new(0.5, 1.3, 1.2);
+        let triangle = Triangle::new(p, q, r);
+
+        let intersects = intersects_aabb_triangle(&aabb, &triangle);
+
+        assert!(!intersects);
+    }
+
+    #[test]
+    fn test_aabb_triangle_fail_axis_e0_y02() {
+        let aabb = get_aabb();
+        let p = Vector3::new(1.1, 0.5, 0.9);
+        let q = Vector3::new(0.8, 0.5, 1.5);
+        let r = Vector3::new(1.3, 0.5, 1.2);
+        let triangle = Triangle::new(p, q, r);
+
+        let intersects = intersects_aabb_triangle(&aabb, &triangle);
+
+        assert!(!intersects);
+    }
+
+    #[test]
+    fn test_aabb_triangle_fail_axis_e0_z12() {
+        let aabb = get_aabb();
+        let p = Vector3::new(1.1, 0.9, 0.5);
+        let q = Vector3::new(0.8, 1.5, 0.5);
+        let r = Vector3::new(1.3, 1.2, 0.5);
+        let triangle = Triangle::new(p, q, r);
+
+        let intersects = intersects_aabb_triangle(&aabb, &triangle);
+
+        assert!(!intersects);
+    }
+
+    #[test]
+    fn test_aabb_triangle_fail_axis_e1_x01() {
+        let aabb = get_aabb();
+        let p = Vector3::new(0.5, 1.3, 1.2);
+        let q = Vector3::new(0.5, 1.1, 0.9);
+        let r = Vector3::new(0.5, 0.8, 1.5);
+        let triangle = Triangle::new(p, q, r);
+
+        let intersects = intersects_aabb_triangle(&aabb, &triangle);
+
+        assert!(!intersects);
+    }
+
+    #[test]
+    fn test_aabb_triangle_fail_axis_e1_y02() {
+        let aabb = get_aabb();
+        let p = Vector3::new(1.3, 0.5, 1.2);
+        let q = Vector3::new(1.1, 0.5, 0.9);
+        let r = Vector3::new(0.8, 0.5, 1.5);
+        let triangle = Triangle::new(p, q, r);
+
+        let intersects = intersects_aabb_triangle(&aabb, &triangle);
+
+        assert!(!intersects);
+    }
+
+    #[test]
+    fn test_aabb_triangle_fail_axis_e1_z0() {
+        let aabb = get_aabb();
+        let p = Vector3::new(1.3, 1.2, 0.5);
+        let q = Vector3::new(1.1, 0.9, 0.5);
+        let r = Vector3::new(0.8, 1.5, 0.5);
+        let triangle = Triangle::new(p, q, r);
+
+        let intersects = intersects_aabb_triangle(&aabb, &triangle);
+
+        assert!(!intersects);
+    }
+
+    #[test]
+    fn test_aabb_triangle_fail_axis_e2_x2() {
+        let aabb = get_aabb();
+        let p = Vector3::new(0.5, 0.8, 1.5);
+        let q = Vector3::new(0.5, 1.3, 1.2);
+        let r = Vector3::new(0.5, 1.1, 0.9);
+        let triangle = Triangle::new(p, q, r);
+
+        let intersects = intersects_aabb_triangle(&aabb, &triangle);
+
+        assert!(!intersects);
+    }
+
+    #[test]
+    fn test_aabb_triangle_fail_axis_e2_y1() {
+        let aabb = get_aabb();
+        let p = Vector3::new(0.8, 0.5, 1.5);
+        let q = Vector3::new(1.3, 0.5, 1.2);
+        let r = Vector3::new(1.1, 0.5, 0.9);
+        let triangle = Triangle::new(p, q, r);
+
+        let intersects = intersects_aabb_triangle(&aabb, &triangle);
+
+        assert!(!intersects);
+    }
+
+    #[test]
+    fn test_aabb_triangle_fail_axis_e2_z12() {
+        let aabb = get_aabb();
+        let p = Vector3::new(0.8, 1.5, 0.5);
+        let q = Vector3::new(1.3, 1.2, 0.5);
+        let r = Vector3::new(1.1, 0.9, 0.5);
+        let triangle = Triangle::new(p, q, r);
+
+        let intersects = intersects_aabb_triangle(&aabb, &triangle);
+
+        assert!(!intersects);
+    }
 }
